c4e4898f6a86d41aa845613bf0c26db2b012eb3c
thaibaotb <dangthaibao14@gmail.com>
2025-09-16 21:21:02 +0700

    Câu 22

---
+++ src/bai22.ts
@@ -0,0 +1,26 @@
+class Stack<T> {
+    private items: T[] = [];
+  
+    push(item: T) {
+      this.items.push(item);
+    }
+  
+    pop(): T | undefined {
+      return this.items.pop();
+    }
+  
+    peek(): T | undefined {
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty(): boolean {
+      return this.items.length === 0;
+    }
+  }
+  
+  const stack = new Stack<number>();
+  stack.push(5);
+  stack.push(10);
+  console.log("Peek:", stack.peek());
+  console.log("Pop:", stack.pop());
+  
